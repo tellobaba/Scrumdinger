76bdb6332d7bd0630bda78e4780726f5e6ad850e
Oluwatomiwa <olatunjitello17@gmail.com>
2023-01-30 15:46:20 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Oluwatomiwa on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
